d779cbbdf5b21cd0af24a2e7bd8d661e090a24cc
Chester Alan <chesteralantagudin@gmail.com>
2020-10-16 03:49:44 +0800

    Updates

--- pages/product/_slug.vue
+++ pages/product/_slug.vue
@@ -8,20 +8,18 @@
 <script>
 export default {
   async asyncData ({ params, payload }) {
-    // eslint-disable-next-line
-    console.log(payload)
     if (payload) {
       return {
         productData: payload,
         product: {
-          title: payload.attributes.title,
-          summary: payload.attributes.summary,
-          price: payload.attributes.price,
-          type: payload.attributes.type,
-          img1: payload.attributes.img_link,
-          img2: payload.attributes.img_link2,
-          img3: payload.attributes.img_link3,
-          img4: payload.attributes.img_link4
+          title: payload.attributes.title ?? '',
+          summary: payload.attributes.summary ?? '',
+          price: payload.attributes.price ?? '',
+          type: payload.attributes.type ?? '',
+          img1: payload.attributes.img_link ?? '',
+          img2: payload.attributes.img_link2 ?? '',
+          img3: payload.attributes.img_link3 ?? '',
+          img4: payload.attributes.img_link4 ?? ''
         }
       }
     } else {
